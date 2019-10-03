3cd9125fcbcfbbf78bb384e76c0bd5cd0ba07ec9
William Jackson <william@subtlecoolness.com>
2019-10-03 14:47:54 -0500

    Use double quotes in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@
 USER_SPEC="${USER_SPEC:-1000}"
 DATABASE="${DATABASE-/database.db}"
 
-chown ${USER_SPEC} "${DATABASE}"
+chown "${USER_SPEC}" "${DATABASE}"
 exec /sbin/su-exec "${USER_SPEC}" /usr/local/bin/python /groupme-backup/groupme_backup.py
